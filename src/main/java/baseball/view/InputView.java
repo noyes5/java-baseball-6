dadb350c82a4b43c027f089f2c9c633f6832379a
noyes5 <noyes5@naver.com>
2023-11-24 20:17:52 +0900

    refactor(InputView): 입력 메시지를 받는 로직 리팩토링

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,10 +8,25 @@ import java.util.List;
 
 public class InputView {
     public List<Integer> readUserNumber() {
+        System.out.print(Message.INPUT_MESSAGE.message);
         String input = Console.readLine();
         validateUserNumber(input);
+        return parseUserInputToNumbers(input);
+    }
+
+    private List<Integer> parseUserInputToNumbers(String input) {
         return Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    private enum Message {
+        INPUT_MESSAGE("숫자를 입력해주세요 : ");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
     }
 }
